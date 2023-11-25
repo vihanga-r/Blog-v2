2425023a4912a54e542836947a8a8455ba9514f2
Vihanga Pandigamage <vihanga.pandigamage@gmail.com>
2023-11-26 02:07:07 +0530

    finalize code and fix bugs. ready to push

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://admin-vihanga:kurulla2k23!@cluster0.lktvjmn.mongodb.net/postsDB');
 
 const app = express();
-let postArray = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -19,86 +18,56 @@ app.use(express.static("public"));
 const postSchema = new mongoose.Schema({
     title: String,
     content: String
-    // can use a kebabCaseTitle if needed
 });
 
 // new model for posts
 const Post = mongoose.model("Post", postSchema);
 
-// GET routes
+
+
+//////////////////////////////// GET routes ///////////////////////////////////////////////////////////////////////////////////////
 
 app.get("/", function(req, res) {
     
     Post.find({}).exec().then(data => {
-        postArray = [];
-        data.forEach(function(post) {
-            postArray.push(post);
-        });
+        
         res.render("home", {
             homeContent: homeStartingContent,
-            postArray: postArray,
+            postArray: data,
         });
-    });
-
-    
+    });    
 });
 
 
-app.get("/posts/:postName", function(req, res) {
+app.get("/posts/:postId", function(req, res) {
 
-    let requestedTitle = req.params.postName; 
+    let requestedId = req.params.postId; 
+    console.log("requestedId: " + requestedId);
 
-    console.log(requestedTitle);
-
-    Post.findOne({title: requestedTitle}).exec().then(post => {
-        //console.log("Searching DB for " + postTitle);
+    Post.findOne({_id: requestedId}).exec().then(post => {
+        
         if (post === null) {
-            console.log("No blog post found by that title");
-            console.log("Therefore rendering 404...");
             res.render("404");
         } else {
-            console.log("A blog post exists by this title! rendering...");
             res.render("post", {
                 postTitle: post.title,
                 postContent: post.content
             });
         }
+    })
+    .catch((err) => {
+        console.log(err);
+        // res.render("status");
     });
 });
-
-
-// app.get("/posts/:postName", function(req, res) {
-
-//     let foundPost = false;
-//     let requestedTitle = _.kebabCase(req.params.postName);
-//     console.log(requestedTitle);
-    
-
-//     for (const post of postArray) {
-//         let storedTitle = _.kebabCase(post.title);
-        
-//         if (storedTitle === requestedTitle) {
-//             res.render("post", {
-//                 postTitle: post.title,
-//                 postContent: post.content
-//             });
-//             foundPost = true;
-//             break;
-//         }   
-//     };
-
-//     if (foundPost === false) {
-//         console.log(requestedTitle + " not equal to " + storedTitle);
-//         res.render("404");
-//     }
-// });
 
 app.get("/compose", function(req, res) {
 
     res.render("compose");
 });
 
-// POST routes
+
+////////////////////////////////// POST routes ///////////////////////////////////////////////////////////////////////////////////
 
 app.post("/compose", function(req, res) {
 
@@ -112,13 +81,11 @@ app.post("/compose", function(req, res) {
     });
 
     Post.findOne({title: postTitle}).exec().then(data => {
-        //console.log("Searching DB for " + postTitle);
+        
         if (data === null) {
-            console.log("No blog post found by that title");
-            console.log("Therefore saving the new post...");
             entry.save();
         } else {
-            console.log("A blog post already exist by this title!");
+            // status.ejs
         }
         res.redirect("/");
     });
